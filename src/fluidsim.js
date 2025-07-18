015c2f8e6109c200ff8a94d22484385b9a63a4a4
sanine <sanine.not@pm.me>
2025-07-18 13:31:06 -0500

    get fluid sim working

--- src/fluidsim.js
+++ src/fluidsim.js
@@ -39,7 +39,7 @@ void main() {
     n = normalize(v);
   }
   vec2 a = texture(field, s_uv-vec2(dx*n.x, dy*n.y)).xy;
-  frag = vec4(mix(v, a, 0.1*dt), 0, 1);
+  frag = vec4(mix(v, 0.99*a, 20.0*dt), 0, 1);
 }
 `;
 
@@ -48,27 +48,33 @@ const stripDivShader = `#version 300 es
 precision highp float;
 in vec2 s_uv;
 uniform sampler2D field;
+uniform sampler2D source;
 uniform float dx;
 uniform float dy;
 out vec4 frag;
 
 void main() {
-  vec2 v0 = texture(field, s_uv+vec2(-dx, -dy)).xy;
+  vec4 vs = texture(source, s_uv);
+  if (vs.a > 0.0) {
+    frag = vec4(vs.xy, 0, 1);
+    return;
+  }
+  vec2 v0 = texture(field, s_uv+vec2( dx, -dy)).xy;
   vec2 v1 = texture(field, s_uv+vec2(  0, -dy)).xy;
-  vec2 v2 = texture(field, s_uv+vec2( dx, -dy)).xy;
+  vec2 v2 = texture(field, s_uv+vec2(-dx, -dy)).xy;
 
-  vec2 v3 = texture(field, s_uv+vec2(-dx,   0)).xy;
+  vec2 v3 = texture(field, s_uv+vec2( dx,   0)).xy;
   vec2 v4 = texture(field, s_uv+vec2(  0,   0)).xy;
-  vec2 v5 = texture(field, s_uv+vec2( dx,   0)).xy;
+  vec2 v5 = texture(field, s_uv+vec2(-dx,   0)).xy;
 
-  vec2 v6 = texture(field, s_uv+vec2(-dx,  dy)).xy;
+  vec2 v6 = texture(field, s_uv+vec2( dx,  dy)).xy;
   vec2 v7 = texture(field, s_uv+vec2(  0,  dy)).xy;
-  vec2 v8 = texture(field, s_uv+vec2( dx,  dy)).xy;
+  vec2 v8 = texture(field, s_uv+vec2(-dx,  dy)).xy;
 
   float x = 
     v0.x-v0.y - (2.0*v1.x) + v2.x+v2.y +
     (2.0*v3.x) - (4.0*v4.x) + (2.0*v5.x) +
-    v6.x+v6.y + (2.0*v7.x) + v8.x-v8.y;
+    v6.x+v6.y - (2.0*v7.x) + v8.x-v8.y;
 
   float y = 
     -v0.x+v0.y + (2.0*v1.y) + v2.x+v2.y +
@@ -78,7 +84,6 @@ void main() {
   frag = vec4(v4 + 0.125*vec2(x, y), 0, 1);
 }
 `;
-
 
 
 
@@ -147,8 +152,8 @@ export const createSim = (gl, w, h) => {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
 
     gl.useProgram(advectProgram);
-    gl.uniform1i(gl.getUniformLocation(advectProgram, 'field'), field);
-    gl.uniform1i(gl.getUniformLocation(advectProgram, 'source'), source);
+    gl.uniform1i(gl.getUniformLocation(advectProgram, 'field'), 0);
+    gl.uniform1i(gl.getUniformLocation(advectProgram, 'source'), 1);
     gl.uniform1f(gl.getUniformLocation(advectProgram, 'dx'), 1/w);
     gl.uniform1f(gl.getUniformLocation(advectProgram, 'dy'), 1/h);
     gl.uniform1f(gl.getUniformLocation(advectProgram, 'dt'), dt);
@@ -168,7 +173,7 @@ export const createSim = (gl, w, h) => {
   }
 
 
-  const stripDiv = (field) => {
+  const stripDiv = (field, source) => {
     gl.bindFramebuffer(gl.FRAMEBUFFER, fg().fb);
 
     gl.viewport(0, 0, w, h);
@@ -176,12 +181,15 @@ export const createSim = (gl, w, h) => {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
 
     gl.useProgram(stripDivProgram);
-    gl.uniform1i(gl.getUniformLocation(stripDivProgram, 'field'), field);
+    gl.uniform1i(gl.getUniformLocation(stripDivProgram, 'field'), 0);
+    gl.uniform1i(gl.getUniformLocation(stripDivProgram, 'source'), 1);
     gl.uniform1f(gl.getUniformLocation(stripDivProgram, 'dx'), 1/w);
     gl.uniform1f(gl.getUniformLocation(stripDivProgram, 'dy'), 1/h);
 
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, field);
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, source);
 
     plane.draw();
 
@@ -193,9 +201,9 @@ export const createSim = (gl, w, h) => {
   const step = (source, dt) => {
     swapBuffers();
     advect(bg().field, source, dt);
-    for (let i=0; i<10; i++) {
+    for (let i=0; i<20; i++) {
       swapBuffers();
-      stripDiv(bg().field);
+      stripDiv(bg().field, source);
     }
   }
 
--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import { createSim } from './fluidsim.js';
 
 window.onload = () => {
   const canvas = document.createElement('canvas');
-  canvas.width = 512;
-  canvas.height = 512;
+  canvas.width = 1024;
+  canvas.height = 1024;
+
+  const canvas2 = document.createElement('canvas');
+  canvas2.width = 512;
+  canvas2.height = 512;
 
 
   const gl = canvas.getContext('webgl2');
+  const ctx = canvas2.getContext('2d');
 
   if (gl == null) {
     alert("Unable to initialize WebGL!");
@@ -27,32 +32,52 @@ window.onload = () => {
   // importExtension("OES_texture_float");
   importExtension("OES_texture_float_linear");
   document.body.append(canvas);
+  document.body.append(canvas2);
 
-  const W = 10;
-  const H = 6;
+
+  const W = 256;
+  const H = 256;
   const sim = createSim(gl, W, H);
 
   const sourceData = [ ...Array(H) ].map(() => [ ...Array(W) ].map(() => [ 0, 0, 0, 0 ]));
   const set = (x, y, v) => sourceData[y][x] = [ ...v, 0, 1 ];
-  set(4, 2, [ 0, 1 ]);
-  set(4, 3, [ 0, 1 ]);
-  set(5, 2, [ 0, 1 ]);
-  set(5, 3, [ 0, 1 ]);
+  for (let x=0; x<W; x++) {
+    set(x,   0, [ 0, 0 ]);
+    set(x, H-1, [ 0, 0 ]);
+  }
+  for (let y=0; y<H; y++) {
+    set(0,   y, [ 0, 0 ]); 
+    set(W-1, y, [ 0, 0 ]); 
+  }
+
+  const cy = Math.floor(H/2);
+  set(1, cy, [ 8, 0 ]);
+  set(1, cy+1, [ 8, 0 ]);
+  set(2, cy, [ 8, 0 ]);
+  set(2, cy+1, [ 8, 0 ]);
+
+  set(H-1, cy+10, [ -8, 0 ]);
+  set(H-1, cy+11, [ -8, 0 ]);
+  set(H-2, cy+10, [ -8, 0 ]);
+  set(H-2, cy+11, [ -8, 0 ]);
+
+
+
   const source = gl.createTexture();
   console.log(sourceData);
   gl.bindTexture(gl.TEXTURE_2D, source);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA32F, W, H, 0, gl.RGBA, gl.FLOAT, new Float32Array(sourceData.flat().flat()));
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
 
   const plane = createObject(gl, [2, 2],
-    [ -0.75, -0.75,   0, 0,
-       0.75, -0.75,   1, 0,
-      -0.75,  0.75,   0, 1,
-       0.75,  0.75,   1, 1,
+    [ -1.0, -1.0,   0, 0,
+       1.0, -1.0,   1, 0,
+      -1.0,  1.0,   0, 1,
+       1.0,  1.0,   1, 1,
     ], [ 0, 1, 3, 0, 3, 2 ],
   );
 
@@ -99,50 +124,96 @@ window.onload = () => {
       float angle = 180.0 + 180.0*atan(v.y, v.x) / 3.141592;
       float r = length(v);
 
-      frag = vec4(hsv2rgb(vec3(angle, 1.0, r)), 1.0);
+      // frag = vec4(hsv2rgb(vec3(angle, 1.0, r)), 1.0);
+      frag = vec4(0, r-1.0, r, 1);
     }`
   );
 
 
 
-  let ms = 0;
+  let ms = null;
   const render = (now) => {
-    const dt = (now - ms)/1000;
+    const dt = ms ? (now - ms)/1000 : 1/60;
     // console.log(`${Math.floor(1/dt)} fps`);
     ms = now;
 
-    sim.stripDiv(source);
-    for (let i=0; i<10; i++) {
+    /*
+    sim.stripDiv(sim.bg().field, source);
+    for (let i=0; i<100; i++) {
       sim.swapBuffers();
-      sim.stripDiv(sim.bg().field);
+      sim.stripDiv(sim.bg().field, source);
     }
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, sim.fg().fb);
+    const pixels = new Float32Array(W*H*4);
+    gl.readPixels(0, 0, W, H, gl.RGBA, gl.FLOAT, pixels);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    const vectors = [ ...Array(W*H).keys() ].reduce(
+      (acc, idx) => [ ...acc, [ pixels[4*idx], pixels[4*idx + 1] ] ],
+      [],
+    );
+
+
+    ctx.lineWidth = 0.06;
+    const scale = 64;
+    ctx.setTransform(ctx.canvas.width/W, 0, 0, ctx.canvas.height/H, 0.5*ctx.canvas.width/W, 0.5*ctx.canvas.height/H);
+
+    const drawVector = (x, y, vx, vy) => {
+      if (vx == 0 && vy == 0) {
+        return;
+      }
+      const [ tx, ty ] = [ x+vx, y+vy ];
+      const angle = Math.atan2(vy, vx);
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(x+vx, y+vy);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(tx - 0.25*Math.cos(angle+1), ty - 0.25*Math.sin(angle+1));
+      ctx.lineTo(tx, ty);
+      ctx.lineTo(tx - 0.25*Math.cos(angle-1), ty - 0.25*Math.sin(angle-1));
+      ctx.stroke();
+    }
+
+    const drawField = (field) => field.forEach((v, idx) => {
+      const x = idx % W;
+      const y = Math.floor(idx / W);
+      drawVector(x, y, v[0], v[1], 0.1);
+      console.log(x, y, v);
+    });
+
+    // drawField(vectors);
+
+    // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    // gl.clearColor(0, 0, 1, 1);
+    // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
+
+    // gl.useProgram(program);
+    // gl.bindTexture(gl.TEXTURE_2D, sim.fg().field);
+    // // gl.bindTexture(gl.TEXTURE_2D, source);
+    // plane.draw();
+    //*/
+
+    
+    sim.step(source, dt);
+    // sim.advect(sim.bg().field, source, 1/60);
+
+    console.log(Math.floor(1/dt));
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0, 0, 1, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
-
-    gl.useProgram(program);
-    gl.bindTexture(gl.TEXTURE_2D, sim.fg().field);
-    // gl.bindTexture(gl.TEXTURE_2D, source);
-    plane.draw();
-
-    /*
-    sim.step(source, dt);
-    // sim.advect(sim.bg().field, source, 1/60);
-
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     gl.useProgram(program);
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, sim.fg().field);
     plane.draw();
     gl.bindTexture(gl.TEXTURE_2D, null);
 
-    // requestAnimationFrame(render);
     //*/
+    requestAnimationFrame(render);
   }
 
-  // requestAnimationFrame(render);
-  render(0);
+  requestAnimationFrame(render);
 
   // gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
   // gl.viewport(0, 0, W, H);
